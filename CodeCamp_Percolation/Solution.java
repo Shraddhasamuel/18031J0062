3e0ee72a0b6f07d1e5ed4188a485ae8b743f2b8a
Shraddhasamuel <43529730+Shraddhasamuel@users.noreply.github.com>
2018-10-03 17:56:05 +0530

    "Solution"

---
+++ CodeCamp_Percolation/Solution.java
@@ -0,0 +1,146 @@
+package Per;
+
+import java.util.Scanner;
+
+class Percolation {
+
+	public int n;
+	public boolean[][] id;
+	public Percolation(int n)
+	{
+		id=new boolean[n][n];
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+			id[i][j]=false;
+			System.out.println("Value:" +i +j +id[i][j]);
+			}
+		}
+	}
+	
+	public void open(int row, int column)
+	{
+				id[row][column]=true;
+				System.out.println(id[row][column]);
+	}
+	
+	public boolean isOpen(int row, int col)  
+
+	{
+				if(id[row][col]==true)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+	}
+	
+	public boolean isFull(int row, int col)  
+
+	{
+				if(id[row][col]==true)
+				{
+					return true;
+				}
+				else
+				{
+				return false;
+				}
+	}
+	
+	public int numberOfOpenSites()
+
+	{
+		return n;	
+	}
+	
+	public boolean percolates() 
+
+	{
+		return true;
+	}
+}
+
+class Weighted
+{
+	public int id[][];
+	int row,col;
+	public int root(int i)
+	{
+		
+		while(i!=id[row][col])
+			i=id[row][col];
+		return i;
+	}
+	
+	public boolean connected(int row,int col)
+	{
+		
+		return root(row)==root(col);
+	}
+	
+	public void union(int p,int q)
+	{
+		int i=root(p);
+		int j=root(q);
+		id[i][j]=j;
+	}
+}
+
+class Solution
+{
+	public static void main(String[] args) throws Exception {
+		Scanner sc=new Scanner(System.in);
+		Scanner sc1=new Scanner(System.in);
+		System.out.println("Enter size:");
+		int size=sc.nextInt();
+		Weighted w=new Weighted();
+		Percolation p=new Percolation(size);
+		
+		while(sc.nextBoolean())
+		{
+			System.out.println("Enter row: ");
+			int r=sc1.nextInt();
+			System.out.println("Enter column: ");
+			int c=sc1.nextInt();
+			p.open(r,c);
+		}
+
+		for(int i=0;i<size;i++)
+		{
+			for(int j=0;j<size;j++)
+			{
+				w.connected(i,j);
+			}
+		}
+		
+		for(int i=0;i<size;i++)
+		{
+			for(int j=0;j<size;j++)
+			{
+				System.out.println("Open: " +p.isOpen(i,j));
+			}
+		}
+		
+		for(int i=0;i<size;i++)
+		{
+			for(int j=0;j<size;j++)
+			{
+				if(p.isFull(i,j)==true)
+				{ 
+					System.out.println("Full");
+					System.out.println("Percolates: " +p.percolates());
+				}
+				else
+				{
+					System.out.println("Does not percolate" );
+				}
+			}
+		}
+		sc.close();
+		sc1.close();
+	}
+}
