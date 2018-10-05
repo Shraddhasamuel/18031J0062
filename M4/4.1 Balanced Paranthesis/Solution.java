cab8e508b5d085e5c5f9924b778e08c298dbf625
Shraddhasamuel <43529730+Shraddhasamuel@users.noreply.github.com>
2018-10-05 15:52:31 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- M4/4.1 Balanced Paranthesis/Solution.java
+++ M4/4.1 Balanced Paranthesis/Solution.java
@@ -1,6 +1,4 @@
-// Java program for checking 
-// balanced Parenthesis 
-  
+
 public class Solution 
 { 
     static class stack  
@@ -41,8 +39,7 @@ public class Solution
         } 
     } 
       
-    /* Returns true if character1 and character2 
-       are matching left and right Parenthesis */
+   
     static boolean isMatchingPair(char character1, char character2) 
     { 
        if (character1 == '(' && character2 == ')') 
@@ -55,40 +52,22 @@ public class Solution
          return false; 
     } 
       
-    /* Return true if expression has balanced  
-       Parenthesis */
+    
     static boolean areParenthesisBalanced(char exp[]) 
     { 
-       /* Declare an empty character stack */
        stack st=new stack(); 
        
-       /* Traverse the given expression to  
-          check matching parenthesis */
        for(int i=0;i<exp.length;i++) 
        { 
-            
-          /*If the exp[i] is a starting  
-            parenthesis then push it*/
           if (exp[i] == '{' || exp[i] == '(' || exp[i] == '[') 
-            st.push(exp[i]); 
-       
-          /* If exp[i] is an ending parenthesis  
-             then pop from stack and check if the  
-             popped parenthesis is a matching pair*/
+            st.push(exp[i]);
           if (exp[i] == '}' || exp[i] == ')' || exp[i] == ']') 
           { 
-                   
-              /* If we see an ending parenthesis without  
-                 a pair then return false*/
              if (st.isEmpty()) 
                { 
                    return false; 
                }  
-       
-             /* Pop the top element from stack, if  
-                it is not a pair parenthesis of character  
-                then there is a mismatch. This happens for  
-                expressions like {(}) */
+ 
              else if ( !isMatchingPair(st.pop(), exp[i]) ) 
                { 
                    return false; 
@@ -97,27 +76,24 @@ public class Solution
             
        } 
           
-       /* If there is something left in expression  
-          then there is a starting parenthesis without  
-          a closing parenthesis */
-        
        if (st.isEmpty()) 
-         return true; /*balanced*/
+         return true;
        else
-         {   /*not balanced*/
+         {   
              return false; 
          }  
     }  
-      
-    /* UTILITY FUNCTIONS */
-    /*driver program to test above functions*/
+
     public static void main(String[] args)  
     { 
-        char exp[] = {};
+        char exp[] = new char[2000] ;
+        for(int i=0;i<exp.length;i++)
+        {
           if (areParenthesisBalanced(exp)) 
             System.out.println("YES"); 
           else
-            System.out.println("NO ");   
+            System.out.println("NO");
+        }
     } 
   
 } 
