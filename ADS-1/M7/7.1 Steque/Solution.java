cf5e1ccb372231c748ad218c2dc78f21c0246ea1
Shraddhasamuel <43529730+Shraddhasamuel@users.noreply.github.com>
2018-10-12 11:07:33 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

---
+++ ADS-1/M7/7.1 Steque/Solution.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+class Node
+{
+	Node front,rear;
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data=d;
+		front=null;
+		rear=null;
+		next=null;
+	}
+}
+class Steque
+{
+	private Node front,rear;
+	
+	Steque()
+	{
+		front=null;
+		rear=null;
+	}
+	
+void push(int d)
+{
+	if(rear==null)
+	{
+		rear=new Node(d);
+	}
+	else
+	{
+		rear=rear.next;
+		rear=new Node(d);
+	}
+}
+
+int pop()
+{
+	if(front==null)
+	{
+		System.out.println("Empty");
+	}
+	else
+	{
+		front=null;
+		front=front.next;
+	}
+	return 0;
+}
+
+void enqueue(int d)
+{
+	if(rear==null)
+	{
+		rear=new Node(d);
+	}
+	else
+	{
+		rear=rear.next;
+		rear=new Node(d);
+	}
+}
+
+}
+
+class Solution
+{
+	public static void main(String args[])
+	{
+		Steque st=new Steque();
+		Scanner sc=new Scanner(System.in);
+		int i=sc.nextInt();
+		String ch=sc.nextLine();
+	}
+}
