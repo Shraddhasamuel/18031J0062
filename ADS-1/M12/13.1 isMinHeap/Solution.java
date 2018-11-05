db507baeac17c450da014a9505fb40c0a8dff61f
Shraddhasamuel <43529730+Shraddhasamuel@users.noreply.github.com>
2018-11-05 21:13:36 +0530

    "check_style_score -> 3 of 5 passed. style score: 0/1 "

--- ADS-1/M12/13.1 isMinHeap/Solution.java
+++ ADS-1/M12/13.1 isMinHeap/Solution.java
@@ -1,231 +1,148 @@
 import java.util.Scanner;
-class Solution 
+
+public class Solution {
+
+	public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	String s1=s.nextLine();
+	switch(s1)
+	{
+	case "String":int n=s.nextInt();
+	              String s2[]=new String[n];
+	              String s01=s.nextLine();
+	              for(int i = 0;s.hasNext();i++)
+	              {
+	            	 String s3=s.next();
+	            
+	            	 String s4[]=null;
+	            	 s4=s3.split(",");
+	            	
+	            	 Minheap m=new Minheap();
+	            	System.out.println(m.comparestring(s4));
+	              }
+	              break;
+	case "Integer":
+		int n1=s.nextInt();
+        String s21[]=new String[n1];
+        String s011=s.nextLine();
+        
+        for(int i = 0;s.hasNext();i++)
+        {
+      	 String s3=s.next();
+      
+      	 String s4[]=null;
+      	 s4=s3.split(",");
+      	
+      	 Minheap m=new Minheap();
+      	System.out.println(m.compare(s4));
+        }
+        break;
+	case "Double":
+		int n11=s.nextInt();
+        String s211[]=new String[n11];
+        String s0111=s.nextLine();
+        
+        for(int i = 0;s.hasNext();i++)
+        {
+      	 String s3=s.next();
+      
+      	 String s4[]=null;
+      	 s4=s3.split(",");
+      	
+      	 Minheap m=new Minheap();
+      	System.out.println(m.compare(s4));
+        }
+        break;
+        
+	case "Float":
+		int n111=s.nextInt();
+        String s2111[]=new String[n111];
+        String s01111=s.nextLine();
+        
+        for(int i = 0;s.hasNext();i++)
+        {
+      	 String s3=s.next();
+      
+      	 String s4[]=null;
+      	 s4=s3.split(",");
+      	
+      	 Minheap m=new Minheap();
+      	System.out.println(m.compare(s4));
+        }
+        break;
+	}
+
+	}
+
+}
+class Minheap
 {
-	static boolean stringheap(int[] str1,int i,int n)
+	public boolean comparestring(String arr[])
+	{int c=0;
+		for(int i = 0;i<arr.length-1;)
+		{
+			int c1=arr[i].charAt(0);
+			int c2=arr[i+1].charAt(0);
+			
+			if(c1<=c2)
+			{
+				i++;
+				c++;
+				
+			}
+			else
+			{
+				
+				c=0;
+				break;
+			}
+		
+		
+		
+		}
+		if(c==arr.length-1)
+		{
+			return true;
+		}
+		else
+		{
+		return false;
+		}
+		
+}
+	public boolean compare(String arr[])
 	{
-		int largest = i;
-        int l = 2*i + 1; 
-        int r = 2*i + 2;
-        if (l < n && (str1[l]>(str1[largest])))
-        {
-            largest = l; 
-        }
-        if (r < n && str1[r]>(str1[largest]))
-        {
-            largest = r; 
-        }
-        /*if (largest != i) 
-        { 
-            int swap = str1[i]; 
-            str1[i] = str1[largest]; 
-            str1[largest] = swap; 
-            
-        }*/
-        stringheap(str1, n, largest); 
-        return true;
-     }
+		int c=0;
+		for(int i = 0;i<arr.length-1;)
+		{
+			double d1=(double)Integer.parseInt(arr[i]);
+			double d2=(double)Integer.parseInt(arr[i+1]);
+			if(d1<=d2)
+			{
+				i++;
+				c++;
+				
+			}
+			else
+			{
+				
+				c=0;
+				break;
+			}
+		
+		
+		
+		}
+		if(c==arr.length-1)
+		{
+			return true;
+		}
+		else
+		{
+		return false;
+		}
+	}
+		
+		
 	
-	static boolean stringheapsort(int[] s,int n)
-	{
-		for(int i=n/2 -1; i>=0; i++ )
-		{
-			stringheap(s,n,i);
-		}
-		for(int i=n-1;i>=0;i++)
-		{
-			int temp=s[0];
-			s[0]=s[1];
-			s[1]=temp;
-			stringheap(s,i,0);
-		}
-		return true;
-	}
-	static boolean intheapsort(int[] s,int n)
-	{
-		for(int i=n/2 -1; i>=0; i++ )
-		{
-			intheap(s,n,i);
-		}
-		for(int i=n-1;i>=0;i++)
-		{
-			int temp=s[0];
-			s[0]=s[1];
-			s[1]=temp;
-			intheap(s,i,0);
-		}
-		return true;
-	}
-	static boolean intheap(int[] a,int i,int n)
-	{
-		int largest = i;
-        int l = 2*i + 1; 
-        int r = 2*i + 2;
-        if (l < n && (a[l]>(a[largest])))
-        {
-            largest = l; 
-        }
-        if (r < n && a[r]>(a[largest]))
-        {
-            largest = r; 
-        }
-        if (largest != i) 
-        { 
-            int swap = a[i]; 
-            a[i] = a[largest]; 
-            a[largest] = swap; 
-           stringheap(a, n, largest); 
-        }
-        return true;
-	}
-        static boolean floatheapsort(float[] s,int n)
-    	{
-    		for(int i=n/2 -1; i>=0; i++ )
-    		{
-    			floatheap(s,n,i);
-    		}
-    		for(int i=n-1;i>=0;i++)
-    		{
-    			float temp=s[0];
-    			s[0]=s[1];
-    			s[1]=temp;
-    			floatheap(s,i,0);
-    		}
-    			return true;
-    	}
-    	static boolean floatheap(float[] a,int i,int n)
-    	{
-    		int largest = i;
-            int l = 2*i + 1; 
-            int r = 2*i + 2;
-            if (l < n && (a[l]>(a[largest])))
-            {
-                largest = l; 
-            }
-            if (r < n && a[r]>(a[largest]))
-            {
-                largest = r; 
-            }
-            if (largest != i) 
-            { 
-                float swap = a[i]; 
-                a[i] = a[largest]; 
-                a[largest] = swap; 
-                floatheap(a, n, largest); 
-            }
-            return true;
-	}
-    	
-    	static boolean doubleheapsort(Double[] aar,int n)
-    	{
-    		for(int i=n/2 -1; i>=0; i++ )
-    		{
-    			doubleheap(aar,n,i);
-    		}
-    		for(int i=n-1;i>=0;i++)
-    		{
-    			double temp=aar[0];
-    			aar[0]=aar[1];
-    			aar[1]=temp;
-    			doubleheap(aar,i,0);
-    		}
-    		return true;
-    	}
-    	static boolean doubleheap(Double[] a,int i,int n)
-    	{
-    		int largest = i;
-            int l = 2*i + 1; 
-            int r = 2*i + 2;
-            if (l < n && (a[l]>(a[largest])))
-            {
-                largest = l; 
-            }
-            if (r < n && a[r]>(a[largest]))
-            {
-                largest = r; 
-            }
-            if (largest != i) 
-            { 
-                double swap = a[i]; 
-                a[i] = a[largest]; 
-                a[largest] = swap; 
-               doubleheap(a, n, largest); 
-            }
-            return true;
-    	}
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		while(true)
-		{
-			String ch=sc.nextLine();
-			switch(ch)
-			{
-			case "String":int a=sc.nextInt();
-			int i=0;
-			while(i!=a)
-			{
-				String str=sc.next();
-				String[] str1=str.split(",");
-				int n=str1.length;
-				int[] ar=new int[n];
-				for(int j=0;j<n;j++)
-				{
-					ar[j]=Integer.parseInt(str1[j]);
-				}
-				System.out.println(stringheapsort(ar,n));
-			}
-			break;
-			
-			case "Integer":int b=sc.nextInt();
-			int q=0;
-			while(q!=b)
-			{
-				String str=sc.next();
-				String[] str1=str.split(",");
-				int n=str1.length;
-				int[] ar=new int[n];
-				for(int j=0;j<n;j++)
-				{
-					ar[j]=Integer.parseInt(str1[j]);
-				}
-				System.out.println(intheapsort(ar,n));
-			}
-			break;
-			
-			case "Float":
-				int c=sc.nextInt();
-				int e=0;
-				while(e!=c)
-				{
-					String str=sc.next();
-					String[] str1=str.split(",");
-					int n=str1.length;
-					float arr[]=new float[n];
-					for(int k=0;k<n;k++)
-					{
-						arr[k]=Float.parseFloat(str1[k]);
-					}
-					System.out.println(floatheapsort(arr,n));
-				}
-			break;
-			
-			case "Double":
-				int d=sc.nextInt();
-				int r=0;
-				while(r!=0)
-				{
-					String str=sc.next();
-					String[] str1=str.split(",");
-					int n=str1.length;
-					Double aar[]=new Double[n];
-					for(int l=0;l<n;l++)
-					{
-						aar[l]=Double.parseDouble(str1[l]);
-					}
-					System.out.println(doubleheapsort(aar,n));
-				}
-			break;	
-			}
-			}
-		}
-	}
+}
--- ADS-1/M14/14.1 Dynamic Median Finding/Solution.java
+++ ADS-1/M14/14.1 Dynamic Median Finding/Solution.java
@@ -1,38 +1,79 @@
-
 import java.util.Scanner;
 
 public class Solution 
 {
-	static void med(int a[])
-	{
-		int n=a.length;
-		int i,b=0;
-		float[] mid=new float[n];
-		for(i=0;i<n;i++)
-		{
-			mid[i]=(b+a[i]) / 2;
-			b=a[i]+b;
-		}
-		for(int k=0;k<n;k++)
-		{
-			System.out.println(mid[k]);
-		}
-	}
-	
-	
 	public static void main(String[] args)
 	{
-	Scanner sc=new Scanner(System.in);
-	while(true)
-	{
-		int n=sc.nextInt();
-		int a[]=new int[n];
-		for(int i=0;i<n;i++)
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int []pq = new int[n];
+		int b = 0;
+		Sort st = new Sort();
+		for (int a = 0; a < n; a++)
 		{
-			a[i]=sc.nextInt();
-			
+			pq[a] = sc.nextInt();
+			b = a + 1;
+			st.sort(pq, b);
+			st.display(pq, b);
 		}
-		med(a);
-	}	
 	}
 }
+	class Sort 
+	{
+		public void sort(int pq[], int b)
+		{
+			int n = b;
+			for (int k = n / 2; k >= 1; k--)
+			{
+				sink(pq, k, n);
+			}
+			while (n > 1) {
+				exch(pq, 1, n--);
+				sink(pq, 1, n);
+			}
+		}
+		void display(int pq[], int b) {
+
+			if (b == 1)
+			{
+				double c = pq[b - 1];
+				System.out.println(c);
+			}
+			else if (b % 2 == 0) {
+				double k = (double) (pq[(b / 2 - 1)] + pq[(b / 2)]) / 2.0;
+				System.out.println(k);
+			}
+			else
+			{
+				double s = pq[(b / 2)];
+				System.out.println(s);
+			}
+					}
+		public  void sink(int pq[], int k, int n)
+		{
+			while ((2 * k) <= n)
+			{
+				int j = 2 * k;
+				if (j < n && less(pq, j, j + 1))
+					j++;
+				{
+					if (!less(pq, k, j))
+						break;
+					exch(pq, k, j);
+					k = j;
+				}
+			}
+		}
+
+		 boolean less(int pq[], int i, int j)
+		{
+			return pq[i - 1] < pq[j - 1];
+		}
+
+		 void exch(int pq[], int n1, int n2) 
+		{
+			int temp = pq[n1 - 1];
+			pq[n1 - 1] = pq[n2 - 1];
+			pq[n2 - 1] = temp;
+		}
+	}
